e9185831b67156191c7cd00e9bc599a2b11cb0a5
Euvin <zotu0987@gmail.com>
2018-07-21 14:49:34 -0700

    updated todolist features

--- todolist/script.js
+++ todolist/script.js
@@ -4,6 +4,8 @@ let input = document.querySelector("input");
 add.addEventListener('click', function() {
     let btn = document.createElement("button");
     btn.innerText = input.value;
+    btn.classList.add("todo-item");
+    btn.style.background = "lightblue";
     if(input.value !== '') {
         document.body.appendChild(btn);
     }
